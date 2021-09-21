72d1e94573b62f85bb3d07c1ff8d8ace66f55742
Arunsh93 <Arunhosur93@gmail.com>
2021-09-21 11:10:11 +0530

    Employee Present Or Absent

---
+++ UC1_PresentOrAbsent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EmployeeAttendanceAndWages
+{
+    class UC1_PresentOrAbsent
+    {
+        static void Main(string[] args)
+        {
+            int is_Full_Time = 1;
+            Random random = new Random();
+            int result = random.Next(0, 2);
+
+            if (result == is_Full_Time)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
